56447d0bacda3c5d21c9d20a1848544c80c9760c
GitanoCosmico <117373513+tomasesquivelgc@users.noreply.github.com>
2023-05-01 16:50:11 -0300

    syntax fix in date.js

--- modules/date.js
+++ modules/date.js
@@ -1,6 +1,8 @@
-const getDate = () =>{
-    const now = luxon.DateTime.now();
-    const formattedDate = now.toFormat('MMMM dth yyyy, h:mm:ss a');
-    document.getElementById('date').textContent = formattedDate;
-}
+import { DateTime } from '../node_modules/luxon/src/luxon.js';
+
+const getDate = () => {
+  const now = DateTime.now();
+  const formattedDate = now.toFormat('MMMM dth yyyy, h:mm:ss a');
+  document.getElementById('date').textContent = formattedDate;
+};
 export default getDate;
